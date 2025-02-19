9344e2f67a3a86f2c9d4ebe884afe74dd9665d7d
etatishev <97806916+etatishev@users.noreply.github.com>
2025-02-19 17:08:28 -0500

    Login sessions and routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,33 @@
 import express from 'express';
+import session from 'express-session';
 import mongoose from 'mongoose';
 import env from './env';
 import cors from 'cors';
 import Game from './models/Game';
+import authRoutes from './routes/auth.js';
 
 mongoose.connect(env.MONGODB_URI);
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.json());
+
+declare module 'express-session' {
+  interface SessionData {
+    player: { id: string; email: string; username: string };
+  }
+}
+
+app.use(
+  session({
+    secret: 'key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+  })
+);
 
 app.get('/', async (_, res) => {
   res.send(
@@ -18,8 +37,14 @@ app.get('/', async (_, res) => {
   );
 });
 
-app.post('/games', async (_, res) => {
-  const game = await Game.create({ players: [] }); // TODO: the logged in user should be added to the players array
+app.use('./auth', authRoutes);
+
+app.post('/games', async (req, res) => {
+  if (!req.session.player) {
+    return void res.status(401).json({ message: 'Not authenticated'});
+  }
+
+  const game = await Game.create({ players: [req.session.player] }); // TODO: the logged in user should be added to the players array
   res.json({ id: game._id });
 });
 
