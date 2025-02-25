0dd115d936afd4357e15d11f7d3aec8cc0c619a0
Megan Liu <mgnliu20@gmail.com>
2025-02-25 13:16:32 -0500

    BE friendlist - player router (accepts api req & calls controller w logic to get the player info)

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -1,3 +1,5 @@
 import Player from '../../models/Player';
 
 export const getAllPlayers = () => Player.find();
+
+export const getPlayer = (pid: string) => Player.findById(pid);
--- backend/src/routes/playersRouter.ts
+++ backend/src/routes/playersRouter.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import { getAllPlayers } from '../controllers/player/playerController';
+import {
+  getAllPlayers,
+  getPlayer
+} from '../controllers/player/playerController';
 // import { requireLoggedInHandler } from './authRouter';
 
 const router = express.Router();
@@ -9,6 +12,12 @@ const router = express.Router();
 
 router.get('/', async (_, res) => {
   res.json(await getAllPlayers());
+});
+
+router.get('/:pid', async (req, res) => {
+  const player = await getPlayer(req.params.pid);
+  if (player === null) return void res.status(404).send('Player not found');
+  return void res.json(player);
 });
 
 export default router;
