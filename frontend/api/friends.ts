4d34241374113d34e318a7a16e81fe908534eb1d
Megan Liu <mgnliu20@gmail.com>
2025-02-26 09:56:16 -0500

    cleaned up API endpoints for friends

--- frontend/api/friends.ts
+++ frontend/api/friends.ts
@@ -1,13 +1,10 @@
 import api from '.';
+import { Player } from './types';
 import { getAllPlayers } from './players';
 
 export const getFriends = async (friendIds: string[]) => {
   const allPlayers = await getAllPlayers();
   return allPlayers.data.filter((p) => friendIds.includes(p._id));
-};
-
-export const addFriend = async (playerid: string, friendId: string) => {
-  api.put(`/players/${playerid}/friend/${friendId}`);
 };
 
 export const removeFriend = async (playerid: string, friendId: string) => {
