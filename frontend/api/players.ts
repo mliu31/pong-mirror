3448792d0046376d2ca400404d0d2faaec5da44e
Megan Liu <mgnliu20@gmail.com>
2025-02-26 09:56:16 -0500

    cleaned up API endpoints for friends

--- frontend/api/players.ts
+++ frontend/api/players.ts
@@ -1,6 +1,4 @@
 import api from '.';
 import { Player } from './types';
 
-export const getAllPlayers = () => api.get<Player>(`/players`);
-
-export const getPlayer = (pid: string) => api.get<Player>(`/players/${pid}`);
+export const getAllPlayers = () => api.get<Player[]>(`/players`);
