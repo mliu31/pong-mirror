6cf59789914fa9b3419222e19c755f128b80b185
Megan Liu <mgnliu20@gmail.com>
2025-02-28 13:34:30 -0500

    refactor: cleaned up getting friends/nonfriends list

--- frontend/api/friends.ts
+++ frontend/api/friends.ts
@@ -6,11 +6,3 @@ export const getFriends = async (friendIds: string[]) => {
   const allPlayers = await getAllPlayers();
   return allPlayers.data.filter((p) => friendIds.includes(p._id));
 };
-
-export const addFriend = async (playerid: string, friendId: string) => {
-  api.put(`/players/${playerid}/friend/${friendId}`);
-};
-
-export const removeFriend = async (playerid: string, friendId: string) => {
-  api.delete(`/players/${playerid}/friend/${friendId}`);
-};
