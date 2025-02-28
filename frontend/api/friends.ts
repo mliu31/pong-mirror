85771985e44eb8666ac0d4b9b0cadc5fbefd5521
Megan Liu <mgnliu20@gmail.com>
2025-02-28 13:34:30 -0500

    refactor: cleaned up getting friends/nonfriends list

--- frontend/api/friends.ts
+++ frontend/api/friends.ts
@@ -6,8 +6,3 @@ export const getFriends = async (friendIds: string[]) => {
   const allPlayers = await getAllPlayers();
   return allPlayers.data.filter((p) => friendIds.includes(p._id));
 };
-
-export const getNonFriends = async (friendIds: string[]) => {
-  const allPlayers = await api.get<Player[]>('/players');
-  return allPlayers.data.filter((p) => !friendIds.includes(p._id));
-};
