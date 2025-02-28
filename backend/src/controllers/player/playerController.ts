39af0c22ecba9b55ce2f036f1e675f3a81ba4ec8
Megan Liu <mgnliu20@gmail.com>
2025-02-28 18:44:55 -0500

    feat: BE add/remove friends -- waiting on pid of currently logged in user

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -3,3 +3,45 @@ import Player from '../../models/Player';
 export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
+u;
+export const addPlayerFriend = async (pid: string, fid: string) => {
+  const player = await getPlayer(pid);
+  const friend = await getPlayer(fid);
+
+  if (!player) {
+    throw new Error(`Player with ID ${pid} not found`);
+  }
+
+  if (!friend) {
+    throw new Error(`Friend with ID ${fid} not found`);
+  }
+
+  const updatedPlayer = await Player.findByIdAndUpdate(
+    pid,
+    { $addToSet: { friends: fid } },
+    { new: true }
+  );
+
+  return updatedPlayer;
+};
+
+export const removePlayerFriend = async (pid: string, fid: string) => {
+  const player = await getPlayer(pid);
+  const friend = await getPlayer(fid);
+
+  if (!player) {
+    throw new Error(`Player with ID ${pid} not found`);
+  }
+
+  if (!friend) {
+    throw new Error(`Friend with ID ${fid} not found`);
+  }
+
+  const updatedPlayer = await Player.findByIdAndUpdate(
+    pid,
+    { $pull: { friends: fid } },
+    { new: true }
+  );
+
+  return updatedPlayer;
+};
