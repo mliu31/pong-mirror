354e40587379424f0d9ac485d8456e21d110a270
Megan Liu <mgnliu20@gmail.com>
2025-02-28 18:44:55 -0500

    feat: BE add/remove friends -- waiting on pid of currently logged in user

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -30,8 +30,44 @@ export const newPlayer = async (name: string, email: string) => {
 export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
+export const addPlayerFriend = async (pid: string, fid: string) => {
+  const player = await getPlayer(pid);
+  const friend = await getPlayer(fid);
 
-// export const getFriends = (pid: string) => {
-//   const toReturn = Player.findById(pid);
-//   return toReturn.friends;
-// };
+  if (!player) {
+    throw new Error(`Player with ID ${pid} not found`);
+  }
+
+  if (!friend) {
+    throw new Error(`Friend with ID ${fid} not found`);
+  }
+
+  const updatedPlayer = await Player.findByIdAndUpdate(
+    pid,
+    { $addToSet: { friends: fid } },
+    { new: true }
+  );
+
+  return updatedPlayer;
+};
+
+export const removePlayerFriend = async (pid: string, fid: string) => {
+  const player = await getPlayer(pid);
+  const friend = await getPlayer(fid);
+
+  if (!player) {
+    throw new Error(`Player with ID ${pid} not found`);
+  }
+
+  if (!friend) {
+    throw new Error(`Friend with ID ${fid} not found`);
+  }
+
+  const updatedPlayer = await Player.findByIdAndUpdate(
+    pid,
+    { $pull: { friends: fid } },
+    { new: true }
+  );
+
+  return updatedPlayer;
+};
--- backend/src/routes/playersRouter.ts
+++ backend/src/routes/playersRouter.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import {
   getAllPlayers,
-  getPlayer
+  getPlayer,
+  addPlayerFriend,
+  removePlayerFriend
 } from '../controllers/player/playerController';
 
 
@@ -19,7 +21,23 @@ router.get('/', async (_, res) => {
 router.get('/:pid', async (req, res) => {
   const player = await getPlayer(req.params.pid);
   if (player === null) return void res.status(404).send('Player not found');
+
   return void res.json(player);
+});
+
+router.put('/:pid/friend/:fid', async (req, res) => {
+  const updatedPlayer = await addPlayerFriend(req.params.pid, req.params.fid);
+
+  res.json(updatedPlayer);
+});
+
+router.delete('/:pid/friend/:fid', async (req, res) => {
+  const updatedPlayer = await removePlayerFriend(
+    req.params.pid,
+    req.params.fid
+  );
+
+  res.json(updatedPlayer);
 });
 
 export default router;
