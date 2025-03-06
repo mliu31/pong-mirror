961958f3a687b7b1cec038a04cd071366aba5dcc
Teddy Wavle <81589383+JetWavs@users.noreply.github.com>
2025-03-06 11:30:08 -0500

    Update playerController.ts

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -66,14 +66,4 @@ export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
 
-export const addFriend = async (playerId: string, friendId: string) => {
-  const friend = await Player.findById(friendId);
-  if (!friend) {
-    throw new Error('Friend not found');
-  }
-  return Player.findByIdAndUpdate(
-    playerId,
-    { $push: { friends: friendId } },
-    { new: true } // Return the updated document
-  );
 };
