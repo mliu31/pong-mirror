3dd41bc99cace6ffa840bec238df75efd42b4c49
Jordan Mann <jordaloo@gmail.com>
2025-03-06 12:11:40 -0500

    fix: assets declarations should include imgs

---
+++ frontend/assets/assets.d.ts
@@ -0,0 +1,9 @@
+declare module '*.ttf' {
+  const value: import('expo-font').FontSource;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
