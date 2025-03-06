73dde41f8504c7a509ffe7bbc4e8a89687a74724
Megan Liu <mgnliu20@gmail.com>
2025-03-06 11:45:18 -0500

    added error handling/status returns with result (from teddy's pfp BE)

--- backend/src/routes/playersRouter.ts
+++ backend/src/routes/playersRouter.ts
@@ -17,25 +17,50 @@ router.get('/', async (_, res) => {
 });
 
 router.get('/:pid', async (req, res) => {
-  const player = await getPlayer(req.params.pid);
-  if (player === null) return void res.status(404).send('Player not found');
-
-  return void res.json(player);
+  try {
+    const player = await getPlayer(req.params.pid);
+    return void res.status(200).json(player);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      return void res.status(404).json({ message: 'Error finding player' });
+    }
+  }
 });
 
 router.put('/:pid/friend/:fid', async (req, res) => {
-  const updatedPlayer = await addPlayerFriend(req.params.pid, req.params.fid);
-
-  res.json(updatedPlayer);
+  try {
+    const updatedPlayer = await addPlayerFriend(req.params.pid, req.params.fid);
+    res.status(200).json(updatedPlayer);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({
+        message: 'An unknown error occurred when adding friend'
+      });
+    }
+  }
 });
 
 router.delete('/:pid/friend/:fid', async (req, res) => {
-  const updatedPlayer = await removePlayerFriend(
-    req.params.pid,
-    req.params.fid
-  );
+  try {
+    const updatedPlayer = await removePlayerFriend(
+      req.params.pid,
+      req.params.fid
+    );
 
-  res.json(updatedPlayer);
+    res.status(200).json(updatedPlayer);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({
+        message: 'An unknown error occurred when removing friend'
+      });
+    }
+  }
 });
 
 export default router;
