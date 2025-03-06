a9a0397db38cf58d7b743d87cb10a9e86cf7fcec
Teddy Wavle <f004vtz@dartmouth.edu>
2025-03-05 19:51:08 -0500

    addFriend backend
    Added and tested (using Postman) backend API endpoints for adding a friend

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -3,3 +3,15 @@ import Player from '../../models/Player';
 export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
+
+export const addFriend = async (playerId: string, friendId: string) => {
+  const friend = await Player.findById(friendId);
+  if (!friend) {
+    throw new Error('Friend not found');
+  }
+  return Player.findByIdAndUpdate(
+    playerId,
+    { $push: { friends: friendId } },
+    { new: true } // Return the updated document
+  );
+};
