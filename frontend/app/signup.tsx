dda0e95fd2a5053992d6bde057f7837878330bb3
Liza Tatishev <97806916+etatishev@users.noreply.github.com>
2025-03-07 17:20:46 -0500

    Login/signup frontend (#54)
    * signup/login simple frontend
    
    * navigation fix, router
    
    * router
    
    * router

--- frontend/app/signup.tsx
+++ frontend/app/signup.tsx
@@ -3,14 +3,7 @@ import { useState } from 'react';
 import { Octicons } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { signup } from '../redux/slices/authSlice';
-import { AppDispatch, RootState } from '../redux/store';
-
 import { useRouter } from 'expo-router';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../redux/store';
-import { signup } from '../redux/slices/authSlice';
 
 const styles = StyleSheet.create({
   container: {
@@ -53,35 +46,9 @@ const styles = StyleSheet.create({
 
 export default function SignUp() {
   const router = useRouter();
-  const dispatch = useDispatch<AppDispatch>();
-  const dispatch = useDispatch<AppDispatch>();
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-
-  const handleSignUp = async () => {
-    if (name && email) {
-      console.log('Attempting to sign up with:', { name, email });
-      try {
-        await dispatch(
-          signup({
-            name,
-            email,
-            _id: '',
-            friends: [],
-            elo: 0
-          })
-        ).unwrap();
-        console.log('Sign up successful');
-
-        router.push('/profile');
-      } catch (err) {
-        console.error('Sign up failed:', err);
-      }
-    } else {
-      console.error('Name and email are required');
-    }
-  };
 
   return (
     <View style={styles.container}>
@@ -114,7 +81,7 @@ export default function SignUp() {
 
       {/* sign up button */}
       <View style={styles.buttonWrapper}>
-        <Button title="Sign up" onPress={handleSignUp} />
+        <Button title="Sign up" />
       </View>
 
       {/* Already have an account? login button */}
