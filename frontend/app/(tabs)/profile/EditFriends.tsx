03701d4e2b5463a3c8379a89c63bd31df75bef82
Megan Liu <mgnliu20@gmail.com>
2025-03-08 00:52:04 -0500

    reload friendslist on pageload of Friends

--- frontend/app/(tabs)/profile/EditFriends.tsx
+++ frontend/app/(tabs)/profile/EditFriends.tsx
@@ -1,6 +1,7 @@
 import { getAllPlayers } from '@/api/players';
 import { Player } from '@/api/types';
 import { useFocusEffect, useLocalSearchParams } from 'expo-router';
+import { useFocusEffect, useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, FlatList } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -15,21 +16,22 @@ export default function EditFriend() {
   }>();
 
   // state for friend ids and sorted list of friends/nonfriends
+
+  // state for friend ids and sorted list of friends/nonfriends
   const [fids, setFids] = useState<Set<string>>(new Set());
   const [sortedPlayers, setSortedPlayers] = useState<Player[]>([]);
 
   // on page load
+  // on page load
   useEffect(() => {
     const newFids = new Set(JSON.parse(friendIds) as string[]);
 
-    // update state of friend ids set
-    setFids(newFids);
+    // update state as set of friend ids
+    setFids(new Set(newFids));
 
-    //  sorts all players into friends and nonfriends
     getAllPlayers().then((res) => {
-      // remove current user
-      const players = res.data.filter((player: Player) => player._id !== pid);
-
+      const players = res.data;
+      //  sorts all players into friends and nonfriends
       players.sort((a: Player, b: Player) => {
         const aIsFriend = newFids.has(a._id);
         const bIsFriend = newFids.has(b._id);
