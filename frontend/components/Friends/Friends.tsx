17c8ffc77d6302a7defa6d3406010afe2f9a8ff2
Megan Liu <mgnliu20@gmail.com>
2025-03-08 00:52:04 -0500

    reload friendslist on pageload of Friends

--- frontend/components/Friends/Friends.tsx
+++ frontend/components/Friends/Friends.tsx
@@ -1,16 +1,23 @@
-import { Player } from '@/api/types';
 import FriendList from './FriendList';
-import { router } from 'expo-router';
+import { router, useFocusEffect } from 'expo-router';
 import { useState } from 'react';
 import { Button, StyleSheet, View, Text } from 'react-native';
+import { getPlayer } from '@/api/players';
 
-const Friends = ({ fids, pid }: { fids: string[]; pid: string }) => {
-  const [friends, setFriends] = useState<Player[]>();
+const Friends = ({ pid }: { fids: string[]; pid: string }) => {
+  // friend ids state
+  const [friends, setFriends] = useState<string[]>([]);
 
-  // useEffect = () => {
+  // on page load, fetch friend ids
+  useFocusEffect(() => {
+    const fetchFriends = async () => {
+      const player = await getPlayer(pid);
+      setFriends(player.data.friends);
+    };
+    fetchFriends();
+  });
 
-  // } []
-
+  // route to edit friends page
   const EditFriendHandler = (fids: string[], pid: string) => {
     router.push({
       pathname: '/profile/EditFriends',
@@ -21,9 +28,9 @@ const Friends = ({ fids, pid }: { fids: string[]; pid: string }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Friends</Text>
-      <FriendList fids={fids} />
+      <FriendList fids={friends} />
       <Button
-        onPress={() => EditFriendHandler(fids, pid)}
+        onPress={() => EditFriendHandler(friends, pid)}
         title="Edit Friends"
       />
     </View>
