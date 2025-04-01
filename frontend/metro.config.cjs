c5d28892448e5c6bca8b96d6a7eed5d9b5035c8c
Jordan Mann <jordaloo@gmail.com>
2025-04-01 11:56:47 -0400

    chore: okay just get it working

---
+++ frontend/metro.config.cjs
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { getDefaultConfig } = require('expo/metro-config');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { withNativeWind } = require('nativewind/metro');
+
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './global.css' });
