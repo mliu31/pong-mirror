c56ef64a67ae33f6523623cae744aa2fa75bd352
Ethan Child <echild@Ethans-MacBook-Pro.local>
2025-04-02 20:34:15 -0400

    landing page

--- frontend/app/(tabs)/community/index.tsx
+++ frontend/app/(tabs)/community/index.tsx
@@ -1,8 +1,10 @@
-// for loading in the landing page, I'll need:
-// load the friends
-// API call to the backend asking for friends.
-// load the groups
-// API call to the backend asking for groups
+import { View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AddButton from './addButton';
+import FriendBox from './friendBox';
+import GroupBox from './groupBox';
+import HomeView from './homeView';
+import TitleDiv from './titleDiv';
 
 // frontend layout
 // span with friends
@@ -12,3 +14,30 @@
 // span with groups
 // button with add groups
 // loaded groups
+import { StyleSheet, Button } from 'react-native';
+
+import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+import { createGame } from '@/api/games';
+import { useRouter } from 'expo-router';
+
+export default function CommunityLandingScreen() {
+  return (
+    <SafeAreaProvider>
+      <View>
+        <TitleDiv />
+      </View>
+      <AddButton category={'Friends'} />
+      <FriendBox rank={1} name={'Ethan Child'} elo={10000} />
+      <FriendBox rank={1} name={'Ethan Child'} elo={10000} />
+      <FriendBox rank={1} name={'Ethan Child'} elo={10000} />
+
+      <AddButton category={'Groups'} />
+      <GroupBox groupName="COSC 98!" />
+      <GroupBox groupName="COSC 98!" />
+      <GroupBox groupName="COSC 98!" />
+      <GroupBox groupName="COSC 98!" />
+
+    </SafeAreaProvider>
+  );
+}
