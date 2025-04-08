9a0118d39f032ad1b69d4f79b0539648e3425575
Ethan Child <echild@Ethans-MacBook-Pro.local>
2025-04-08 10:27:21 -0400

    commenting out

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -66,7 +66,7 @@ export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
 
-export const getFriends = (pid: string) => {
-  const toReturn = Player.findById(pid);
-  return toReturn.friends;
-};
+// export const getFriends = (pid: string) => {
+//   const toReturn = Player.findById(pid);
+//   return toReturn.friends;
+// };
