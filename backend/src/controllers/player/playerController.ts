6ec374d6b026da2b218921aae3b6a56b7f936757
Ethan Child <echild@Ethans-MacBook-Pro.local>
2025-04-08 10:24:04 -0400

    updated name

--- backend/src/controllers/player/playerController.ts
+++ backend/src/controllers/player/playerController.ts
@@ -3,3 +3,8 @@ import Player from '../../models/Player';
 export const getAllPlayers = () => Player.find();
 
 export const getPlayer = (pid: string) => Player.findById(pid);
+
+export const getFriends = (pid: string) => {
+  const toReturn = Player.findById(pid);
+  return toReturn.friends;
+};
---
+++ frontend/app/(tabs)/community/addButton.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet, View, TouchableHighlight } from 'react-native';
+import { Dimensions } from 'react-native';
+import { useRouter } from 'expo-router';
+import { ThemedText } from '@/components/ThemedText';
+
+interface AddButtonProps {
+  category: string;
+}
+
+const styles = StyleSheet.create({
+  buttonStyling: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: Dimensions.get('window').width / 10,
+    marginLeft: Dimensions.get('window').width / 10,
+    width: Dimensions.get('window').width / 20,
+    height: Dimensions.get('window').height / 20,
+    backgroundColor: 'blue',
+    borderRadius: 5
+  },
+  titleStyling: {
+    alignSelf: 'flex-start',
+    fontSize: 25,
+    paddingLeft: Dimensions.get('window').height / 20,
+    paddingRight: Dimensions.get('window').height / 20,
+    flex: 1,
+    textAlign: 'left',
+    textAlignVertical: 'center'
+  },
+  viewStyling: {
+    marginTop: Dimensions.get('window').height / 20,
+    flexDirection: 'row'
+  }
+});
+
+const AddButton: React.FC<AddButtonProps> = ({ category }) => {
+  const router = useRouter();
+  if (category === 'Friends') {
+    return (
+      <>
+        <View style={styles.viewStyling}>
+          <ThemedText style={styles.titleStyling}>Friends</ThemedText>
+          <TouchableHighlight
+            style={styles.buttonStyling}
+            onPress={() => router.push('./addFriends')}
+          >
+            <ThemedText>+</ThemedText>
+          </TouchableHighlight>
+        </View>
+        <View></View>
+      </>
+    );
+  } else if (category === 'Groups') {
+    return (
+      <View style={styles.viewStyling}>
+        <ThemedText style={styles.titleStyling}>Groups</ThemedText>
+        <TouchableHighlight
+          style={styles.buttonStyling}
+          onPress={() => router.push('./addGroup')}
+        >
+          <ThemedText>+</ThemedText>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+};
+
+export default AddButton;
