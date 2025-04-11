bb5ce6e7f942507a13f2357278e8ca019238f04c
Megan Liu <mgnliu20@gmail.com>
2025-04-10 23:17:44 -0400

    feat: split screen vertically for team building

--- frontend/app/(tabs)/game/[gameid]/teamBuilder.tsx
+++ frontend/app/(tabs)/game/[gameid]/teamBuilder.tsx
@@ -4,13 +4,16 @@ import { ThemedText } from '@/components/ThemedText';
 import { useEffect, useState } from 'react';
 import { getGame } from '@/api/games';
 import TeamChoiceButtons from '../../../../components/TeamChoiceButtons';
-import { Button } from 'react-native';
+import { Button, ButtonText } from '@/components/ui/button';
 import { Game } from '@/api/apiTypes';
+import { Box } from '@/components/ui/box';
+import { Text } from '@/components/ui/text';
 
 export default function Route() {
   const local = useLocalSearchParams();
 
   const [gameData, setGameData] = useState<Game | null>(null);
+  const [continueButtonDisabled, setContinueButtonDisabled] = useState(false);
 
   useEffect(() => {
     getGame(local.gameid as string).then((res) =>
@@ -23,16 +26,24 @@ export default function Route() {
   };
 
   return (
-    <ThemedView>
+    <ThemedView className="flex-1">
       {gameData === null ? (
         <ThemedText>Loading</ThemedText>
       ) : (
         <>
-          <ThemedText>Game ID: {local.gameid}</ThemedText>
+          <Box className="flex-row h-full">
+            <Box className="w-1/2 bg-success-300 p-10 top-0 left-0 ">
+              <Text className="text-typography-default">Team 1</Text>
+            </Box>
+
+            <Box className="w-1/2 bg-secondary-50 p-10 top-0 right-0">
+              <Text className="text-typography-default">Team 2</Text>
+            </Box>
+          </Box>
 
           {gameData.players.map(({ player, team }) => (
             <ThemedText key={player._id}>
-              Player: {player.name} - Team: {team ?? 'Unassigned'}
+              {player.name}
               <TeamChoiceButtons
                 pid={player._id}
                 initialValue={team}
@@ -40,7 +51,15 @@ export default function Route() {
               />
             </ThemedText>
           ))}
-          <Button title="Create Team" onPress={createTeamHandler} />
+          <Button
+            onPress={createTeamHandler}
+            action="primary"
+            disabled={continueButtonDisabled}
+            variant="solid"
+            size="md"
+          >
+            <ButtonText>Continue</ButtonText>
+          </Button>
         </>
       )}
     </ThemedView>
