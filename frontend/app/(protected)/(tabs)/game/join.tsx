42e9ff78adefa4155ce6ea9bb65cdf658f2b7cc1
Jordan Mann <jordaloo@gmail.com>
2025-04-21 23:58:30 -0400

    feat: qr code scan icon

--- frontend/app/(protected)/(tabs)/game/join.tsx
+++ frontend/app/(protected)/(tabs)/game/join.tsx
@@ -1,15 +1,23 @@
 import { CameraView } from 'expo-camera';
+import { ScanQrCode } from 'lucide-react-native';
+import { View } from 'react-native';
 
 export default function JoinGame() {
   return (
-    <CameraView
-      barcodeScannerSettings={{
-        barcodeTypes: ['qr']
-      }}
-      onBarcodeScanned={(scanningResult) => {
-        // Handle the scanned QR code here
-        console.log(scanningResult.data);
-      }}
-    />
+    <View className="flex-1">
+      <CameraView
+        barcodeScannerSettings={{
+          barcodeTypes: ['qr']
+        }}
+        className="absolute w-full h-full"
+        onBarcodeScanned={(scanningResult) => {
+          // Handle the scanned QR code here
+          console.log(scanningResult.data);
+        }}
+      />
+      <View className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-fit h-fit">
+        <ScanQrCode size="100" />
+      </View>
+    </View>
   );
 }
