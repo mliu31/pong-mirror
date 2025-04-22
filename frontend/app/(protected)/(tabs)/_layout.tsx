e12298885f0da181e95453fba3b1e4044e688a0e
Jordan Mann <jordaloo@gmail.com>
2025-04-22 11:55:03 -0400

    fix: remove home, in progress tabs

--- frontend/app/(protected)/(tabs)/_layout.tsx
+++ frontend/app/(protected)/(tabs)/_layout.tsx
@@ -41,24 +41,6 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="inProgress"
-        options={{
-          title: 'inProgress',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
         name="game"
         options={{
           title: 'Game',
