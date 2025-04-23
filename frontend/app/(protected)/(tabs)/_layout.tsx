c3d8b3b01e9ae6704471f83a4525c8547b974f3c
Megan Liu <mgnliu20@gmail.com>
2025-04-23 11:32:47 -0400

    cleanup: delete backbutton, remove inprogress from root _layout file (was giving warning in console)

--- frontend/app/(protected)/(tabs)/_layout.tsx
+++ frontend/app/(protected)/(tabs)/_layout.tsx
@@ -49,15 +49,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="inProgress"
-        options={{
-          title: 'inProgress',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
         name="game"
         options={{
           title: 'Game',
