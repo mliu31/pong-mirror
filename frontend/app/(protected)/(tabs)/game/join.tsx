81f89269b9e9e14f66f213981beb441aecd33cf4
Jordan Mann <jordaloo@gmail.com>
2025-04-29 11:11:48 -0400

    fix: stop camera when route loses focus

--- frontend/app/(protected)/(tabs)/game/join.tsx
+++ frontend/app/(protected)/(tabs)/game/join.tsx
@@ -1,17 +1,28 @@
 import { Toast, useToast } from '@/components/ui/toast';
 import { CameraView, ScanningResult } from 'expo-camera';
-import { RelativePathString, useRouter } from 'expo-router';
+import { RelativePathString, useNavigation, useRouter } from 'expo-router';
 import { ScanQrCode } from 'lucide-react-native';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { View, Text } from 'react-native';
 import { Linking } from 'react-native';
 
 export default function JoinGame() {
   const router = useRouter();
+  const navigation = useNavigation();
   const toast = useToast();
   const [lastQRValue, setLastQRValue] = useState<string | null>(null);
 
+  const [cameraActive, setCameraActive] = useState(true);
+  useEffect(
+    () =>
+      navigation.addListener('state', () =>
+        setCameraActive(navigation.isFocused())
+      ),
+    [navigation]
+  );
+
   const handleBarcodeScanned = async ({ data }: ScanningResult) => {
+    console.log('handle barcode scanned', data);
     // don't continue to scan the same QR code
     if (data === lastQRValue) {
       return;
@@ -41,15 +52,23 @@ export default function JoinGame() {
     }
   };
 
+  const cameraViewRef = useRef<CameraView>(null);
+
+  // TODO: consider .launchScanner if on iOS
+
   return (
     <View className="flex-1">
-      <CameraView
-        barcodeScannerSettings={{
-          barcodeTypes: ['qr']
-        }}
-        className="absolute w-full h-full"
-        onBarcodeScanned={handleBarcodeScanned}
-      />
+      {/* camera's active prop is not working, just stop rendering the camera */}
+      {cameraActive && (
+        <CameraView
+          ref={cameraViewRef}
+          barcodeScannerSettings={{
+            barcodeTypes: ['qr']
+          }}
+          className="absolute w-full h-full"
+          onBarcodeScanned={handleBarcodeScanned}
+        />
+      )}
       <View className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-fit h-fit">
         <ScanQrCode size="100" />
       </View>
