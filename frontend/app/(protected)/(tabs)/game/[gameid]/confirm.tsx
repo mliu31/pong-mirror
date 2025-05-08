666d0cd6b378a737cf05075748d9a06157ab4ebd
Megan Liu <mgnliu20@gmail.com>
2025-05-08 11:38:48 -0400

    cleanup UI on confirmation page

--- frontend/app/(protected)/(tabs)/game/[gameid]/confirm.tsx
+++ frontend/app/(protected)/(tabs)/game/[gameid]/confirm.tsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Button, ButtonText } from '@/components/ui/button';
 import { ThemedText } from '@/components/ThemedText';
 import { useLocalSearchParams, router } from 'expo-router';
 import { getGameInvites } from '@/api/invite';
 import INVITE from '@/constants/INVITE';
 import { addPlayersToGame } from '@/api/games';
-import { IInvite } from '@/api/types';
+import { IInvite, IPlayer } from '@/api/types';
+import { getPlayer } from '@/api/players';
+import { Box } from '@/components/ui/box';
 
 export default function Confirm() {
   const { gameid } = useLocalSearchParams<{ gameid: string }>();
   const [invites, setInvites] = useState<IInvite[]>([]);
+  const [invitees, setInvitees] = useState<Record<string, IPlayer>>({});
 
+  // fetch invites for game
   useEffect(() => {
     const fetchInvites = async () => {
       const res = await getGameInvites(gameid);
@@ -22,10 +26,24 @@ export default function Confirm() {
     return () => clearInterval(interval);
   }, [gameid]);
 
+  // fetch player info for each invite
+  useEffect(() => {
+    Promise.all(invites.map((i) => getPlayer(i.playerId))).then((players) => {
+      players.forEach((player) => {
+        setInvitees((prev) => ({
+          ...prev,
+          [player.data._id]: player.data
+        }));
+      });
+    });
+  }, [invites]);
+
+  // number of accepted invites
   const acceptedInvitesCount = invites.filter(
     (invite) => invite.status.toString() === INVITE.ACCEPTED
   ).length;
 
+  // add all players to game
   const addPlayersToGameHandler = async () => {
     await addPlayersToGame(
       gameid,
@@ -34,18 +52,42 @@ export default function Confirm() {
     router.push('./teamBuilder');
   };
 
+  // box color for each invite
+  const statusStyles: Record<string, string> = {
+    [INVITE.ACCEPTED]: 'bg-green-700',
+    [INVITE.PENDING]: 'bg-gray-500',
+    [INVITE.DECLINED]: 'bg-red-500'
+  };
+
+  const renderItem = ({ item }: { item: IInvite }) => {
+    return (
+      <Box className="flex flex-row justify-between items-center p-7 pt-0">
+        <ThemedText className="text-base">
+          {invitees[item.playerId]?.name}
+        </ThemedText>
+
+        <Box>
+          <ThemedText
+            className={`${statusStyles[item.status.toString()]} px-3 py-1 rounded-full`}
+          >
+            {item.status.toString()}
+          </ThemedText>
+        </Box>
+      </Box>
+    );
+  };
+
   return (
     <View>
-      {invites.map((invite) => (
-        <ThemedText key={invite.playerId}>
-          Player ID: {invite.playerId}, Status: {invite.status.toString()}
-        </ThemedText>
-      ))}
-      {/* <FlatList
+      <Box className="flex-row p-7 justify-start">
+        <ThemedText className="font-bold text-xl">Player</ThemedText>
+        <ThemedText className="ml-auto font-bold text-xl">Status</ThemedText>
+      </Box>
+      <FlatList
         data={invites}
         keyExtractor={(item) => item.playerId.toString()}
-        renderItem={({ item }) => <Text>Player ID: {item.playerId}</Text>}
-      /> */}
+        renderItem={renderItem}
+      />
       <Button
         onPress={addPlayersToGameHandler}
         disabled={acceptedInvitesCount !== invites.length}
