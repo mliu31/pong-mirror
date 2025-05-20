3da3c1857be6804f1cd26f6046a9221cf40073c1
Jordan Mann <jordaloo@gmail.com>
2025-05-19 22:23:25 -0400

    fix: log out immediately on 401

--- frontend/app/(protected)/_layout.tsx
+++ frontend/app/(protected)/_layout.tsx
@@ -1,5 +1,8 @@
+import NotificationProvider from '@/components/NotificationProvider';
+import { IoProvider } from '@/context/IoContext';
 import { useAppSelector } from '@/redux/redux-hooks';
-import { Redirect, Slot, usePathname } from 'expo-router';
+import { Redirect, Slot, usePathname, useRouter } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function ProtectedLayout() {
   // TODO: flesh out route protection
@@ -7,18 +10,32 @@ export default function ProtectedLayout() {
 
   const pathname = usePathname();
 
+  const router = useRouter();
+
+  useEffect(() => {
+    // the protected route may still be rendering while going to singup, ignore if this is the case;
+    // otherwise next will be signup.
+    if (!basicPlayerInfo && pathname !== '/signup') {
+      console.log('going to signup, will return to', pathname);
+      router.push({
+        pathname: '/signup',
+        params: {
+          next: pathname
+        }
+      });
+    }
+  }, [basicPlayerInfo, pathname, router]);
+
   if (!basicPlayerInfo) {
-    return (
-      <Redirect
-        href={{
-          pathname: '/signup',
-          params: {
-            next: pathname
-          }
-        }}
-      />
-    );
+    // user is not authenticated, show blank page while we wait for the above effect to kick in
+    return null;
   }
 
-  return <Slot></Slot>;
+  return (
+    <IoProvider>
+      <NotificationProvider>
+        <Slot></Slot>
+      </NotificationProvider>
+    </IoProvider>
+  );
 }
